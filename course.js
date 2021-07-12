be36b69c72bd4e2b580efb3200a25d7d72652479
haoyu050735 <69667026+haoyu050735@users.noreply.github.com>
2021-07-12 21:53:29 +0800

    Add files via upload

--- course.js
+++ course.js
@@ -8,6 +8,7 @@ function a06() {window.location.href="n01_tian/a06.html";}
 function a07() {window.location.href="n01_tian/a07.html";} 
 function a08() {window.location.href="n01_tian/a08.html";} 
 function a09() {window.location.href="n01_tian/a09.html";} 
+function a10() {window.location.href="n01_tian/a10.html";} 
 function b01() {window.location.href="n02_di/b01.html";} 
 function b02() {window.location.href="n02_di/b02.html";} 
 function b03() {window.location.href="n02_di/b03.html";} 
