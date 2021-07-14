2faa5beaac1ed3179789ac839777a620a3f1eb03
haoyu050735 <69667026+haoyu050735@users.noreply.github.com>
2021-07-14 23:25:07 +0800

    Update course.js

--- course.js
+++ course.js
@@ -53,3 +53,4 @@ function g08() {window.location.href="n07_yuyan/g08.html";}
 function g09() {window.location.href="n07_yuyan/g09.html";} 
 function g10() {window.location.href="n07_yuyan/g10.html";} 
 function g11() {window.location.href="n07_yuyan/g11.html";} 
+function h01() {window.location.href="n08_other/h01.html";} 
